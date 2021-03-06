ba341b4ee3da0d570c5658575db3a772e10472fd
Texcoco2212 <64113779+Texcoco2212@users.noreply.github.com>
2021-03-07 00:16:49 +0900

    Add files via upload

---
+++ BINGO/index.php
@@ -0,0 +1,50 @@
+<?php
+
+//http://localhost/ビンゴ/index.php
+
+$nums = [];
+
+for($i = 0; $i<5; $i++) {
+  $col = range($i * 15 +1,  $i * 15 +15);
+  shuffle($col);
+  $nums[$i] = array_slice($col, 0, 5);
+}
+
+$nums[2][2] = "FREE" ;
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bingo!</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <table>
+      <tr>
+        <th>B</th>
+        <th>I</th>
+        <th>N</th>
+        <th>G</th>
+        <th>O</th>
+    </tr>
+    <?php  for ( $i = 0; $i < 5; $i ++) : ?>
+      <tr>
+         <?php for ($j = 0; $j <5; $j ++) : ?>
+           <td><?= h($nums[$j][$i]) ; ?></td>
+         <?php endfor; ?>
+      </tr>
+    <?php endfor; ?>
+  </table>
+</div>
+
+</body>
+</html>
